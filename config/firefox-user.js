5086a007c90f0a6d93821b6b2020a8307f4ffe60
mengsokool <patcharaphon143@gmail.com>
2026-04-29 21:36:27 +0700

    feat(kiosk): disable Firefox save password and autofill dialogs via pre-seeded user.js

---
+++ config/firefox-user.js
@@ -0,0 +1,13 @@
+// Kiosk mode profile defaults
+user_pref("signon.rememberSignons", false);
+user_pref("browser.formfill.enable", false);
+user_pref("browser.sessionstore.resume_from_crash", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("app.update.auto", false);
+user_pref("app.update.enabled", false);
+user_pref("browser.translations.enable", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("toolkit.telemetry.enabled", false);
